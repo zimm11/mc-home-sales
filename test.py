aa540c8afb20c26894c23dce730305f02cc5d5b5
Chris Zimmerman <cdzimm118@gmail.com>
2019-11-10 12:06:37 -0500

    update .gitignore

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import numpy as np
 gbr = load('gbr.joblib')
 
 # 'Year Built', 'Floor Area', 'Acres', 'Municipality', 'School Name', '# Bedrooms', '# Bathrooms', '# Half Bathrooms'
-x_new_input = np.array([1980, 1800, 1, 6, 18, 3, 2, 1]).reshape((1, -1))
+x_new_input = np.array([2000, 1800, 1, 6, 18, 3, 2, 1]).reshape((1, -1))
 
 y_new = gbr.predict(x_new_input)
 print(y_new)
