f6bc3c5c8612033b48827fc080974560cf6d4fe7
Chris Zimmerman <cdzimm118@gmail.com>
2019-11-12 16:48:06 -0500

    initial commit for flask and html code

--- app.py
+++ app.py
@@ -1,12 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from joblib import load
+import numpy as np
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/index')
 
+def index():
+    return render_template('index.html')
+
+def prediction(prediction_list):
+    prediction_values = np.array(prediction_list).reshape((1, -1))
+    model = load('gbr.joblib')
+    sale_price = model.predict(prediction_values)
+    return sale_price[0]
+
+
+@app.route('/prediction', methods=['POST'])
+def result():
+    if request.method == 'POST':
+        prediction_list = request.form.to_dict()
+        prediction_list = list(prediction_list.values())
+        prediction_list = list(map(int, prediction_list))
+        sale_price = prediction(prediction_list)
+
+        return render_template("index.html", prediction=sale_price)
 
 if __name__ == '__main__':
     app.run()
